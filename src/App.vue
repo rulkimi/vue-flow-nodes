db5915ce5efc39b78eba6f2ff04e2bbf36fe9778
rulkimi <azrulkimi00@gmail.com>
2024-12-06 15:18:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const addCount = () => {
+  count.value++;
+}
+</script>
+
+<template>
+  <button class="py-1 px-2 rounded-sm bg-teal-500 text-white" @click="addCount">Add Count</button>
+  <h1 class="text-3xl font-bold underline inline ml-2">
+    Count: {{ count }}
+  </h1>
+</template>
