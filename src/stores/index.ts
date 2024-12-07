4bf1477a9907f9d5f5040c922c52e328096a0c37
rulkimi <azrulkimi00@gmail.com>
2024-12-07 13:11:01 +0800

    generate edges based on payload.json data

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -17,12 +17,15 @@ export const useMainStore = defineStore('main', () => {
         switch (node.id) {
             case 1: return { x: 250, y: 0 };
             case 'd09c08': return { x: 250, y: 200 };
+            case '161f52': return { x: 180, y: 350 };
+            case '28c4b9': return { x: 485, y: 350 };
             case 'b6a0c1': return { x: 400, y: 500 };
             case 'b0653a': return { x: 100, y: 500 };
             case 'e879e4': return { x: 400, y: 750 };
             default: return { x: 0, y: 0 };
           }
       })(),
+      draggable: node.type === 'dateTimeConnector' ? false : true
     }));
 
   const nodes = ref();
@@ -35,7 +38,7 @@ export const useMainStore = defineStore('main', () => {
   }
 
   const setNodes = (node: Node[]) => {
-    const filteredNode = node.filter(node => node.name !== 'Success' && node.name !== 'Failure');
+    const filteredNode = node;
     const newNodes = createNodes(filteredNode);
     nodes.value = newNodes;
   }
@@ -51,48 +54,15 @@ export const useMainStore = defineStore('main', () => {
     { deep: true, immediate: true }
   );
 
-  const generateEdges = (nodes: Node[]) => {
-    const edgesList = [];
-    
-    const ids = nodes.map(node => node.id);
-    
-    if (ids.includes(1) && ids.includes('d09c08')) {
-      edgesList.push({
-        id: 'e1-d09c08',
-        source: '1',
-        target: 'd09c08',
-        type: 'button',
-      });
-    }
-
-    if (ids.includes('d09c08') && ids.includes('b0653a')) {
-      edgesList.push({
-        id: 'd09c08-b0653a',
-        source: 'd09c08',
-        target: 'b0653a',
-        type: 'button',
-      });
-    }
-
-    if (ids.includes('d09c08') && ids.includes('b6a0c1')) {
-      edgesList.push({
-        id: 'd09c08-b6a0c1',
-        source: 'd09c08',
-        target: 'b6a0c1',
-        type: 'button',
-      });
-    }
-
-    if (ids.includes('b6a0c1') && ids.includes('e879e4')) {
-      edgesList.push({
-        id: 'b6a0c1-e879e4',
-        source: 'b6a0c1',
-        target: 'e879e4',
-        type: 'button',
-      });
-    }
-
-    return edgesList;
+  const generateEdges = (nodes: Node[]): { id: string; source: string; target: string; type: string }[] => {
+    return nodes
+      .filter((node) => node.parentId && node.parentId !== -1)
+      .map((node) => ({
+        id: `${node.parentId}-${node.id}`,
+        source: `${node.parentId}`,
+        target: `${node.id}`,
+        type: node.type === 'dateTimeConnector' ? 'smoothstep' : 'button',
+      }));
   };
 
   return {
