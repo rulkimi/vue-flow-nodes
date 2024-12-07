5eaf7de6f6d6da3215e31e8d3ee563b1b807f969
rulkimi <azrulkimi00@gmail.com>
2024-12-07 11:01:04 +0800

    only generate edges once nodes are set

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 import { Node } from '../types'
 
@@ -26,50 +26,80 @@ export const useMainStore = defineStore('main', () => {
     }));
 
   const nodes = ref();
-  
-  const edges = ref([
-    {
-      id: 'e1-d09c08',
-      source: '1',
-      target: 'd09c08',
-      type: 'button',
-    },
-    {
-      id: 'd09c08-b0653a',
-      type: 'button',
-      source: 'd09c08',
-      target: 'b0653a',
-    },
-    {
-      id: 'd09c08-b6a0c1',
-      type: 'button',
-      source: 'd09c08',
-      target: 'b6a0c1',
-    },
-    {
-      id: 'b6a0c1-e879e4',
-      type: 'button',
-      source: 'b6a0c1',
-      target: 'e879e4',
-    },
-  ]);
+  const edges = ref();
 
-  const activeNode = ref()
+  const activeNode = ref();
+  
   const setActiveNode = (nodeId: string) => {
     activeNode.value = nodeId;
   }
 
   const setNodes = (node: Node[]) => {
     const filteredNode = node.filter(node => node.name !== 'Success' && node.name !== 'Failure');
-    const newNodes = createNodes(filteredNode)
-    nodes.value = newNodes
+    const newNodes = createNodes(filteredNode);
+    nodes.value = newNodes;
   }
+
+  watch(
+    () => nodes.value,
+    (newNodes) => {
+      if (!newNodes) return;
+
+      const newEdges = generateEdges(newNodes);
+      edges.value = newEdges;
+    },
+    { deep: true, immediate: true }
+  );
+
+  const generateEdges = (nodes: Node[]) => {
+    const edgesList = [];
+    
+    const ids = nodes.map(node => node.id);
+    
+    if (ids.includes(1) && ids.includes('d09c08')) {
+      edgesList.push({
+        id: 'e1-d09c08',
+        source: '1',
+        target: 'd09c08',
+        type: 'button',
+      });
+    }
+
+    if (ids.includes('d09c08') && ids.includes('b0653a')) {
+      edgesList.push({
+        id: 'd09c08-b0653a',
+        source: 'd09c08',
+        target: 'b0653a',
+        type: 'button',
+      });
+    }
+
+    if (ids.includes('d09c08') && ids.includes('b6a0c1')) {
+      edgesList.push({
+        id: 'd09c08-b6a0c1',
+        source: 'd09c08',
+        target: 'b6a0c1',
+        type: 'button',
+      });
+    }
+
+    if (ids.includes('b6a0c1') && ids.includes('e879e4')) {
+      edgesList.push({
+        id: 'b6a0c1-e879e4',
+        source: 'b6a0c1',
+        target: 'e879e4',
+        type: 'button',
+      });
+    }
+
+    return edgesList;
+  };
 
   return {
     nodes,
     edges,
     activeNode,
     setActiveNode,
-    setNodes
+    setNodes,
   };
 });
