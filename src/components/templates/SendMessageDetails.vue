7db91fdd9a554f08e6daba4414e64751aea52752
rulkimi <azrulkimi00@gmail.com>
2024-12-08 20:59:51 +0800

    add option to remove message

--- src/components/templates/SendMessageDetails.vue
+++ src/components/templates/SendMessageDetails.vue
@@ -50,6 +50,11 @@ const addTextMessage = () => {
   });
 };
 
+const deleteMessage = (index: number) => {
+  messages.value.splice(index, 1);
+};
+
+
 const addAttachmentMessage = () => {
   const generatedId = Date.now().toString()
   const newMessage: AttachmentPayload = { id: generatedId, type: 'attachment', attachment: 'No File Chosen' };
@@ -80,85 +85,101 @@ const openUrl = (url: string) => {
 </script>
 
 <template>
-  <label>
-    <p class="text-slate-500 mb-1">Title<sup class="text-red-500">*</sup></p>
-    <input
-      v-model="sendMessageTitle"
-      id="send-message-title"
-      type="text"
-      placeholder="E.g. Welcome Message"
-      class="border rounded-lg px-2 py-1 w-full"
-      @input="updateSendMessageTitleValue"
-    >
-  </label>
-  <div>
-    <p class="text-slate-500 mb-1">Messages<sup class="text-red-500">*</sup></p>
-    <transition-group name="messages" tag="ul" class="flex flex-col gap-2">
-      <div
-        v-for="(message, index) in messages"
-        :key="index"
+  <div class="flex flex-col gap-4">
+    <label>
+      <p class="text-slate-500 mb-1 font-semibold">Title<sup class="text-red-500">*</sup></p>
+      <input
+        v-model="sendMessageTitle"
+        id="send-message-title"
+        type="text"
+        placeholder="E.g. Welcome Message"
+        class="border rounded-lg px-2 py-1 w-full"
+        @input="updateSendMessageTitleValue"
       >
-        <div v-if="message.type === 'text'">
-          <label>
-            <p class="text-slate-500 mb-1 text-sm">Message #{{ index + 1 }} - {{ message.type }}</p>
-            <input
-              :id="message.id"
-              v-model="message.text"
-              class="border rounded-lg px-2 py-1 w-full"
-            />
-          </label>
-        </div>
-        <div v-else-if="message.type === 'attachment'">
-          <label class="cursor-pointer">
-            <p class="text-slate-500 mb-1 text-sm">
-              Message #{{ index + 1 }} - {{ message.type }} 
-              <span v-if="message.attachment !== 'No File Chosen'" class="text-blue-500">・ 
-                <span 
-                  class="hover:underline cursor-pointer"
-                  @click.prevent="
-                    typeof message.attachment === 'string'
-                      ? openUrl(message.attachment)
-                      : openImage(message.attachment)
-                  "
-                >
-                  Preview
+    </label>
+    <div>
+      <p class="text-slate-500 mb-1 font-semibold">Messages<sup class="text-red-500">*</sup></p>
+      <transition-group name="messages" tag="ul" class="flex flex-col gap-4">
+        <div
+          v-for="(message, index) in messages"
+          :key="message.id ?? index"
+        >
+          <div v-if="message.type === 'text'">
+            <label>
+              <p class="text-slate-500 mb-1 text-sm flex justify-between">
+                Message #{{ index + 1 }} - {{ message.type }}
+                <font-awesome-icon
+                  class="text-red-500 cursor-pointer hover:scale-105 transition-transform duration-300"
+                  :icon="['far', 'trash-alt']"
+                  @click.prevent="deleteMessage(index)"
+                />
+              </p>
+              <input
+                :id="message.id"
+                v-model="message.text"
+                class="border rounded-lg px-2 py-1 w-full"
+              />
+            </label>
+          </div>
+          <div v-else-if="message.type === 'attachment'">
+            <label class="cursor-pointer">
+              <p class="text-slate-500 mb-1 text-sm flex justify-between">
+                <span>
+                  Message #{{ index + 1 }} - {{ message.type }}
+                  <span v-if="message.attachment !== 'No File Chosen'" class="text-blue-500">・
+                    <span
+                      class="hover:underline cursor-pointer"
+                      @click.prevent="
+                        typeof message.attachment === 'string'
+                          ? openUrl(message.attachment)
+                          : openImage(message.attachment)
+                      "
+                    >
+                      Preview
+                    </span>
+                  </span>
                 </span>
-              </span>
-            </p>
-            <div class="border rounded-lg px-2 py-1 w-full flex items-center gap-2">
-              <div class="px-1 py-0.5 bg-blue-500/10 rounded-full text-sm text-nowrap">Choose File</div>
-              <span class="text-nowrap overflow-hidden text-ellipsis" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-                {{
-                  typeof message.attachment === 'string'
-                    ? message.attachment
-                    : message.attachment?.name || 'No File Chosen'
-                }}
-              </span>
-            </div>
-            <input
-              :id="message.id"
-              type="file"
-              class="hidden"
-              @change="onFileChange($event, index)"
-            >
-          </label>
+                <font-awesome-icon
+                  class="text-red-500 cursor-pointer hover:scale-105 transition-transform duration-300"
+                  :icon="['far', 'trash-alt']"
+                  @click.prevent="deleteMessage(index)"
+                />
+              </p>
+              <div class="border rounded-lg px-2 py-1 w-full flex items-center gap-2">
+                <div class="px-1 py-0.5 bg-blue-500/10 rounded-full text-sm text-nowrap">Choose File</div>
+                <span class="text-nowrap overflow-hidden text-ellipsis" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                  {{
+                    typeof message.attachment === 'string'
+                      ? message.attachment
+                      : message.attachment?.name || 'No File Chosen'
+                  }}
+                </span>
+              </div>
+              <input
+                :id="message.id"
+                type="file"
+                class="hidden"
+                @change="onFileChange($event, index)"
+              >
+            </label>
+          </div>
         </div>
-      </div>
-      <div key="add-text-attachment" class="bg-blue-500/10 border-2 border-dotted border-blue-500 rounded-lg p-2 flex gap-2">
-        <div
-          class="w-full bg-white px-3 py-2 rounded-lg cursor-pointer hover:scale-105 transition-transform duration-300"
-          @click="addTextMessage"
-        >
-          <font-awesome-icon class="text-slate-500 mr-1" :icon="['fas', 'quote-right']" /> Add Text
+        <div key="add-text-attachment" class="bg-blue-500/10 border-2 border-dotted border-blue-500 rounded-lg p-2 flex gap-2">
+          <div
+            class="w-full bg-white px-3 py-2 rounded-lg cursor-pointer hover:scale-105 transition-transform duration-300"
+            @click="addTextMessage"
+          >
+            <font-awesome-icon class="text-slate-500 mr-1" :icon="['fas', 'quote-right']" /> Add Text
+          </div>
+          <div
+            class="w-full bg-white p-2 rounded-lg cursor-pointer hover:scale-105 transition-transform duration-300"
+            @click="addAttachmentMessage"
+          >
+            <font-awesome-icon class="text-slate-500 mr-1" :icon="['fas', 'paperclip']" /> Add Attachment
+          </div>
         </div>
-        <div
-          class="w-full bg-white p-2 rounded-lg cursor-pointer hover:scale-105 transition-transform duration-300"
-          @click="addAttachmentMessage"
-        >
-          <font-awesome-icon class="text-slate-500 mr-1" :icon="['fas', 'paperclip']" /> Add Attachment
-        </div>
-      </div>
-    </transition-group>
+      </transition-group>
+    </div>
   </div>
 </template>
 
